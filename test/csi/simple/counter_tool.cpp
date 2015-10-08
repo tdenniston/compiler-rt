efeec78e253fd755ba45c5dd549c298af6ac2d0d
Damon Doucet <ddoucet@mit.edu>
2015-10-08 16:17:55 +0000

    Replace branching by num_bytes with array access

--- test/csi/simple/counter_tool.cpp
+++ test/csi/simple/counter_tool.cpp
@@ -1,41 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-long bytes_accessed;
-long shorts_accessed;
-long ints_accessed;
-long longs_accessed;
+long times_accessed_by_size[4];
 
 static void __ok_destroy() {
-    fprintf(stderr, "bytes accessed: %ld\n", bytes_accessed);
-    fprintf(stderr, "shorts accessed: %ld\n", shorts_accessed);
-    fprintf(stderr, "ints accessed: %ld\n", ints_accessed);
-    fprintf(stderr, "longs accessed: %ld\n", longs_accessed);
+    fprintf(stderr, "bytes accessed: %ld\n", times_accessed_by_size[0]);
+    fprintf(stderr, "shorts accessed: %ld\n", times_accessed_by_size[1]);
+    fprintf(stderr, "ints accessed: %ld\n", times_accessed_by_size[2]);
+    fprintf(stderr, "longs accessed: %ld\n", times_accessed_by_size[3]);
 }
 extern "C" void __ok_init() {
     atexit(__ok_destroy);
 }
 extern "C" void __ok_before_load(void *addr, int num_bytes, int attr) {
-    if (num_bytes == 1)
-        bytes_accessed++;
-    else if (num_bytes == 2)
-        shorts_accessed++;
-    else if (num_bytes == 4)
-        ints_accessed++;
-    else if (num_bytes == 8)
-        longs_accessed++;
+    times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
 extern "C" void __ok_after_load(void *addr, int num_bytes, int attr) {
 }
 extern "C" void __ok_before_store(void *addr, int num_bytes, int attr) {
-    if (num_bytes == 1)
-        bytes_accessed++;
-    else if (num_bytes == 2)
-        shorts_accessed++;
-    else if (num_bytes == 4)
-        ints_accessed++;
-    else if (num_bytes == 8)
-        longs_accessed++;
+    times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
 extern "C" void __ok_after_store(void *addr, int num_bytes, int attr) {
 }
