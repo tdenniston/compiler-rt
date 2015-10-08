37410883ecf4c767c5b97f42af447e9c61c18d4b
Damon Doucet <ddoucet@mit.edu>
2015-10-08 15:29:46 +0000

    Convert memory throughput test to use one before/after_load/store per type

--- test/csi/simple/counter_tool.cpp
+++ test/csi/simple/counter_tool.cpp
@@ -15,49 +15,29 @@ static void __ok_destroy() {
 extern "C" void __ok_init() {
     atexit(__ok_destroy);
 }
-extern "C" void __ok_before_load1(void *addr, int attr) {
-    bytes_accessed++;
+extern "C" void __ok_before_load(void *addr, int num_bytes, int attr) {
+    if (num_bytes == 1)
+        bytes_accessed++;
+    else if (num_bytes == 2)
+        shorts_accessed++;
+    else if (num_bytes == 4)
+        ints_accessed++;
+    else if (num_bytes == 8)
+        longs_accessed++;
 }
-extern "C" void __ok_before_load2(void *addr, int attr) {
-    shorts_accessed++;
+extern "C" void __ok_after_load(void *addr, int num_bytes, int attr) {
 }
-extern "C" void __ok_before_load4(void *addr, int attr) {
-    ints_accessed++;
+extern "C" void __ok_before_store(void *addr, int num_bytes, int attr) {
+    if (num_bytes == 1)
+        bytes_accessed++;
+    else if (num_bytes == 2)
+        shorts_accessed++;
+    else if (num_bytes == 4)
+        ints_accessed++;
+    else if (num_bytes == 8)
+        longs_accessed++;
 }
-extern "C" void __ok_before_load8(void *addr, int attr) {
-    longs_accessed++;
-}
-extern "C" void __ok_after_load1(void *addr, int attr) {
-}
-extern "C" void __ok_after_load2(void *addr, int attr) {
-}
-extern "C" void __ok_after_load4(void *addr, int attr) {
-}
-extern "C" void __ok_after_load8(void *addr, int attr) {
-}
-// extern "C" void __ok_after_float(void *addr, int attr) {}
-// extern "C" void __ok_after_double(void *addr, int attr) {}
-extern "C" void __ok_before_store1(void *addr, int attr) {
-    bytes_accessed++;
-}
-extern "C" void __ok_before_store2(void *addr, int attr) {
-    shorts_accessed++;
-}
-extern "C" void __ok_before_store4(void *addr, int attr) {
-    ints_accessed++;
-}
-extern "C" void __ok_before_store8(void *addr, int attr) {
-    longs_accessed++;
-}
-// extern "C" void __ok_before_float(void *addr, int attr) {}
-// extern "C" void __ok_before_double(void *addr, int attr) {}
-extern "C" void __ok_after_store1(void *addr, int attr) {
-}
-extern "C" void __ok_after_store2(void *addr, int attr) {
-}
-extern "C" void __ok_after_store4(void *addr, int attr) {
-}
-extern "C" void __ok_after_store8(void *addr, int attr) {
+extern "C" void __ok_after_store(void *addr, int num_bytes, int attr) {
 }
 extern "C" void __ok_func_entry(void *parentReturnAddr, char *funcName) {
 }
--- test/csi/simple/empty_tool.h
+++ test/csi/simple/empty_tool.h
@@ -3,26 +3,10 @@
 extern "C" {
 void __ok_init();
 
-void __ok_before_load1(void *addr, int attr);
-void __ok_before_load2(void *addr, int attr);
-void __ok_before_load4(void *addr, int attr);
-void __ok_before_load8(void *addr, int attr);
-void __ok_after_load1(void *addr, int attr);
-void __ok_after_load2(void *addr, int attr);
-void __ok_after_load4(void *addr, int attr);
-void __ok_after_load8(void *addr, int attr);
-// void __ok_after_float(void *addr, int attr);
-// void __ok_after_double(void *addr, int attr);
-void __ok_before_store1(void *addr, int attr);
-void __ok_before_store2(void *addr, int attr);
-void __ok_before_store4(void *addr, int attr);
-void __ok_before_store8(void *addr, int attr);
-// void __ok_before_float(void *addr, int attr);
-// void __ok_before_double(void *addr, int attr);
-void __ok_after_store1(void *addr, int attr);
-void __ok_after_store2(void *addr, int attr);
-void __ok_after_store4(void *addr, int attr);
-void __ok_after_store8(void *addr, int attr);
+void __ok_before_load(void *addr, size_t num_bytes, int attr);
+void __ok_after_load(void *addr, size_t num_bytes, int attr);
+void __ok_before_store(void *addr, size_t num_bytes, int attr);
+void __ok_after_store(void *addr, size_t num_bytes, int attr);
 void __ok_func_entry(void *parentReturnAddr, char *funcName);
 void __ok_func_exit();
 }
