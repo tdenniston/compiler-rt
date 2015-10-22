12ac7dfdafbe1dbb78a1cc6cfae90dafdd753586
Damon Doucet <ddoucet@mit.edu>
2015-10-22 20:14:14 +0000

    Fix empty tool to accept the size of a load/store as a parameter rather than using multiple methods

---
+++ test/csi/simple/empty_tool.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void __ok_destroy() {
+    fprintf(stderr, "Destroy tool\n");
+}
+extern "C" void __ok_init() {
+    fprintf(stderr, "Initialize tool\n");
+    atexit(__ok_destroy);
+}
+extern "C" void __ok_before_load(void *addr, int num_bytes, int attr) {
+    fprintf(stderr, "Before load %p (%d bytes) attr %d\n", addr, num_bytes, attr);
+}
+extern "C" void __ok_after_load(void *addr, int num_bytes, int attr) {
+    fprintf(stderr, "After load %p (%d bytes) attr %d\n", addr, num_bytes, attr);
+}
+extern "C" void __ok_before_store(void *addr, int num_bytes, int attr) {
+    fprintf(stderr, "Before store %p (%d bytes) attr %d\n", addr, num_bytes, attr);
+}
+extern "C" void __ok_after_store(void *addr, int num_bytes, int attr) {
+    fprintf(stderr, "After store %p (%d bytes) attr %d\n", addr, num_bytes, attr);
+}
+extern "C" void __ok_func_entry(void *parentReturnAddr, char *funcName) {
+    fprintf(stderr, "Func entry, parent return addr %p func name %s.\n", parentReturnAddr, funcName);
+}
+extern "C" void __ok_func_exit() {
+    fprintf(stderr, "Func exit\n");
+}
