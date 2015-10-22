178f46f885d1f3f5408feb7a3a7e3e61bb2dd067
Damon Doucet <ddoucet@mit.edu>
2015-10-22 21:47:17 +0000

    Rename tool methods from ok to csi, and update Makefile

--- test/csi/simple/counter_tool.cpp
+++ test/csi/simple/counter_tool.cpp
@@ -3,26 +3,26 @@
 
 long times_accessed_by_size[4];
 
-static void __ok_destroy() {
+static void __csi_destroy() {
     fprintf(stderr, "bytes accessed: %ld\n", times_accessed_by_size[0]);
     fprintf(stderr, "shorts accessed: %ld\n", times_accessed_by_size[1]);
     fprintf(stderr, "ints accessed: %ld\n", times_accessed_by_size[2]);
     fprintf(stderr, "longs accessed: %ld\n", times_accessed_by_size[3]);
 }
-extern "C" void __ok_init() {
-    atexit(__ok_destroy);
+extern "C" void __csi_init() {
+    atexit(__csi_destroy);
 }
-extern "C" void __ok_before_load(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_before_load(void *addr, int num_bytes, int attr) {
     times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
-extern "C" void __ok_after_load(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_after_load(void *addr, int num_bytes, int attr) {
 }
-extern "C" void __ok_before_store(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_before_store(void *addr, int num_bytes, int attr) {
     times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
-extern "C" void __ok_after_store(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_after_store(void *addr, int num_bytes, int attr) {
 }
-extern "C" void __ok_func_entry(void *parentReturnAddr, char *funcName) {
+extern "C" void __csi_func_entry(void *parentReturnAddr, char *funcName) {
 }
-extern "C" void __ok_func_exit() {
+extern "C" void __csi_func_exit() {
 }
--- test/csi/simple/empty_tool.cpp
+++ test/csi/simple/empty_tool.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void __ok_destroy() {
+static void __csi_destroy() {
     fprintf(stderr, "Destroy tool\n");
 }
-extern "C" void __ok_init() {
+extern "C" void __csi_init() {
     fprintf(stderr, "Initialize tool\n");
-    atexit(__ok_destroy);
+    atexit(__csi_destroy);
 }
-extern "C" void __ok_before_load(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_before_load(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "Before load %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-extern "C" void __ok_after_load(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_after_load(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "After load %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-extern "C" void __ok_before_store(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_before_store(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "Before store %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-extern "C" void __ok_after_store(void *addr, int num_bytes, int attr) {
+extern "C" void __csi_after_store(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "After store %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-extern "C" void __ok_func_entry(void *parentReturnAddr, char *funcName) {
+extern "C" void __csi_func_entry(void *parentReturnAddr, char *funcName) {
     fprintf(stderr, "Func entry, parent return addr %p func name %s.\n", parentReturnAddr, funcName);
 }
-extern "C" void __ok_func_exit() {
+extern "C" void __csi_func_exit() {
     fprintf(stderr, "Func exit\n");
 }
