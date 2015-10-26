ac3c257a35ba5bc98d7dc47d1402af172348029e
Tyler Denniston <tyler@csail.mit.edu>
2015-10-26 13:45:52 -0400

    Remove WEAK attributes in tool implementation.

--- test/csi/toolkit/counter_tool.cpp
+++ test/csi/toolkit/counter_tool.cpp
@@ -5,22 +5,22 @@
 extern "C" {
 long times_accessed_by_size[4];
 
-WEAK void __csi_destroy() {
+void __csi_destroy() {
     fprintf(stderr, "bytes accessed: %ld\n", times_accessed_by_size[0]);
     fprintf(stderr, "shorts accessed: %ld\n", times_accessed_by_size[1]);
     fprintf(stderr, "ints accessed: %ld\n", times_accessed_by_size[2]);
     fprintf(stderr, "longs accessed: %ld\n", times_accessed_by_size[3]);
 }
 
-WEAK void __csi_init() {
+void __csi_init() {
     atexit(__csi_destroy);
 }
 
-WEAK void __csi_before_load(void *addr, int num_bytes, int attr) {
+void __csi_before_load(void *addr, int num_bytes, int attr) {
     times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
 
-WEAK void __csi_before_store(void *addr, int num_bytes, int attr) {
+void __csi_before_store(void *addr, int num_bytes, int attr) {
     times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
 
--- test/csi/toolkit/print_tool.cpp
+++ test/csi/toolkit/print_tool.cpp
@@ -4,29 +4,29 @@
 
 extern "C" {
 
-WEAK void __csi_destroy() {
+void __csi_destroy() {
     fprintf(stderr, "Destroy tool\n");
 }
-WEAK void __csi_init() {
+void __csi_init() {
     fprintf(stderr, "Initialize tool\n");
     atexit(__csi_destroy);
 }
-WEAK void __csi_before_load(void *addr, int num_bytes, int attr) {
+void __csi_before_load(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "Before load %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-WEAK void __csi_after_load(void *addr, int num_bytes, int attr) {
+void __csi_after_load(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "After load %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-WEAK void __csi_before_store(void *addr, int num_bytes, int attr) {
+void __csi_before_store(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "Before store %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-WEAK void __csi_after_store(void *addr, int num_bytes, int attr) {
+void __csi_after_store(void *addr, int num_bytes, int attr) {
     fprintf(stderr, "After store %p (%d bytes) attr %d\n", addr, num_bytes, attr);
 }
-WEAK void __csi_func_entry(void *parentReturnAddr, char *funcName) {
+void __csi_func_entry(void *parentReturnAddr, char *funcName) {
     fprintf(stderr, "Func entry, parent return addr %p func name %s.\n", parentReturnAddr, funcName);
 }
-WEAK void __csi_func_exit() {
+void __csi_func_exit() {
     fprintf(stderr, "Func exit\n");
 }
 
