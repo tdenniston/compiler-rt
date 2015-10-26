2cbaed6d1a5db87b44b2e0ca0b7f4de16caa85fc
Tyler Denniston <tyler@csail.mit.edu>
2015-10-26 13:01:08 -0400

    Remove empty definitions from counter_tool.
    Now with the null tool and weak symbols, we only have to override functions we
    care about.

--- test/csi/toolkit/counter_tool.cpp
+++ test/csi/toolkit/counter_tool.cpp
@@ -11,22 +11,17 @@ WEAK void __csi_destroy() {
     fprintf(stderr, "ints accessed: %ld\n", times_accessed_by_size[2]);
     fprintf(stderr, "longs accessed: %ld\n", times_accessed_by_size[3]);
 }
+
 WEAK void __csi_init() {
     atexit(__csi_destroy);
 }
+
 WEAK void __csi_before_load(void *addr, int num_bytes, int attr) {
     times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
-WEAK void __csi_after_load(void *addr, int num_bytes, int attr) {
-}
+
 WEAK void __csi_before_store(void *addr, int num_bytes, int attr) {
     times_accessed_by_size[__builtin_ctz(num_bytes)]++;
 }
-WEAK void __csi_after_store(void *addr, int num_bytes, int attr) {
-}
-WEAK void __csi_func_entry(void *parentReturnAddr, char *funcName) {
-}
-WEAK void __csi_func_exit() {
-}
 
-}
+} // extern "C"
