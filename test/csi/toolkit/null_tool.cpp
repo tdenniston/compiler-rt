b1f68859ac21c2ec2e801b22f4060959cf115f76
Tyler Denniston <tyler@csail.mit.edu>
2015-10-26 12:59:31 -0400

    Add null tool

---
+++ test/csi/toolkit/null_tool.cpp
@@ -0,0 +1,14 @@
+#include "csi.h"
+
+extern "C" {
+
+WEAK void __csi_destroy() {}
+WEAK void __csi_init() {}
+WEAK void __csi_before_load(void *addr, int num_bytes, int attr) {}
+WEAK void __csi_after_load(void *addr, int num_bytes, int attr) {}
+WEAK void __csi_before_store(void *addr, int num_bytes, int attr) {}
+WEAK void __csi_after_store(void *addr, int num_bytes, int attr) {}
+WEAK void __csi_func_entry(void *parentReturnAddr, char *funcName) {}
+WEAK void __csi_func_exit() {}
+
+} // extern "C"
