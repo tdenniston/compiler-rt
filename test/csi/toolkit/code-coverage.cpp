50ff205f090532ed0c5ca78da618497cf8df1aff
Tyler Denniston <tyler@csail.mit.edu>
2015-11-12 15:16:18 -0500

    Use debug info in code coverage tool

--- test/csi/toolkit/code-coverage.cpp
+++ test/csi/toolkit/code-coverage.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <cstdio>
+#include <string>
 #include <vector>
 
 namespace {
@@ -48,6 +49,12 @@ public:
         _data[elt] &= ~(1 << off);
     }
 
+    inline bool isset(unsigned bit) {
+        unsigned elt, off;
+        bit2elt(bit, elt, off);
+        return _data[elt] & (1 << off);
+    }
+
     unsigned count() const {
         unsigned result = 0;
         for (unsigned i = 0; i < _nelts; i++) {
@@ -62,6 +69,8 @@ public:
 };
 
 std::vector<Bitset> *bitsets = NULL;
+std::vector<std::string> *filenames = NULL;
+std::vector<std::vector<std::pair<uint32_t, uint32_t>>> *linenumbers = NULL;
 
 void destroy() {
     unsigned num_basic_blocks = 0, num_covered = 0;
@@ -69,6 +78,20 @@ void destroy() {
         num_basic_blocks += bitsets->at(i).size();
         num_covered += bitsets->at(i).count();
     }
+    printf("\Covered code region report:\n\n");
+    printf("%20s | %20s\n", "Line Number Range", "File");
+    printf("-------------------------------------------\n");
+    for (unsigned i = 0; i < bitsets->size(); i++) {
+        std::string filename = filenames->at(i);
+        for (unsigned bb = 0; bb < bitsets->at(i).size(); bb++) {
+            if (bitsets->at(i).isset(bb)) {
+                std::pair<uint32_t, uint32_t> ln = linenumbers->at(i)[bb];
+                printf("% 6d    -- % 6d  | %20s\n", ln.first, ln.second, filename.c_str());
+            }
+        }
+    }
+
+    printf("-------------------------------------------\n");
     printf("Code coverage summary:\n");
     printf("%d/%d (%.02f%%) basic blocks executed.\n", num_covered, num_basic_blocks,
            ((float)num_covered/num_basic_blocks)*100);
@@ -81,16 +104,26 @@ extern "C" {
 void __csi_init(csi_info_t info) {
     atexit(destroy);
     bitsets = new std::vector<Bitset>(info.num_modules);
+    filenames = new std::vector<std::string>(info.num_modules);
+    linenumbers = new std::vector<std::vector<std::pair<uint32_t, uint32_t>>>(info.num_modules);
 }
 
 void __csi_module_init(csi_module_info_t info) {
     assert(bitsets && info.module_id < bitsets->size());
     bitsets->at(info.module_id).allocate(info.num_basic_blocks);
+    linenumbers->at(info.module_id).resize(info.num_basic_blocks);
 }
 
 void __csi_bb_entry(csi_id_t id, const char *filename, uint32_t linestart, uint32_t lineend) {
     assert(bitsets && id.module_id < bitsets->size());
     assert(id.id < (*bitsets)[id.module_id].size());
+    if (!(*bitsets)[id.module_id].isset(id.id)) {
+        (*linenumbers)[id.module_id][id.id] = std::make_pair(linestart, lineend);
+    }
+    if ((*filenames)[id.module_id].empty()) {
+        (*filenames)[id.module_id] = std::string(filename);
+    }
+
     (*bitsets)[id.module_id].set(id.id);
 }
 
